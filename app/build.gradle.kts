ae563d5db018785d2e197d4e49a26f29216485b5
Kerry Shen <kefishen@gmail.com>
2026-06-08 15:08:48 +0800

    chore: bump version to 2.3 (versionCode 5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.shezik.drawanywhere"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.2"
+        versionCode = 5
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
